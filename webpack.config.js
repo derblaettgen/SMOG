56bcfad3ed8c5e6f2cda2a5263c1eb31ae247b54
derblaettgen <sascha@blaettgen.com>
2021-01-16 22:10:03 +0100

    Update Dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,39 @@
-var path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
-  devtool: 'inline-source-map',
+  mode: "development",
+  entry: "./src/app.ts",
+  devtool: "inline-source-map",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.template.ejs',
-      inject: 'body'
-    })
-  ],
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 9000
+    port: 9000,
   },
   module: {
-    rules: [{
+    rules: [
+      {
+        test: /\.html?$/,
+        use: "html-loader",
+      },
+      {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        use: "ts-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           "style-loader", // creates style nodes from JS strings
           "css-loader", // translates CSS into CommonJS
-        ]
-      }
-    ]
+        ],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js']
+    extensions: [".tsx", ".ts", ".js"],
   },
 };
