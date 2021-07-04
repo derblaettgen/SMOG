7d856cb7127b7a7124a13535844f46661276513a
derblaettgen <sascha@blaettgen.com>
2021-07-04 15:57:24 +0200

    Updated Packages And Webpack Config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const CompressionPlugin = require("compression-webpack-plugin");
+const UglifyWebpackPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [new CompressionPlugin({
+    algorithm: "gzip",
+    deleteOriginalAssets: true
+  })],
+  optimization: {
+    minimizer: [new UglifyWebpackPlugin({ sourceMap: false })]
+  }
 });
